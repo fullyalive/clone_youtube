410cf9cac7b083653da4c68d3c2f581f483273a5
fullyalive <wogud1202@gmail.com>
2019-02-06 23:09:52 +0700

    Express로 라우트 관리하기

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ function handleListening() {
   console.log(`Listening on : http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  console.log(req);
+  res.send('hello from home');
+}
+
+function handleProfile(req, res) {
+  console.log(req);
+  res.send('user profile');
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
