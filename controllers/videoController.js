84527d4dc7eb85a91bb9d42ff474811ca841e7d1
fullyalive <wogud1202@gmail.com>
2019-02-08 21:18:28 +0700

    video 업로드/수정 템플릿 | db 정보 받아서 홈에 출력하기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,14 @@
+import { videos } from "../db";
 // render 함수의 첫번째 인자는 template, 두 번째 인자는 템플릿에 추가할 정보가 담긴 객체
 
-export const home = (req, res) => res.render("home", { pageTitle: "홈" }); // 이 함수는 자동으로 views 폴더에서 이름이 home이고 확장자가 pug인 템플릿 파일을 찾아서 보여준다.
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "홈", videos });
+}; // 이 함수는 자동으로 views 폴더에서 이름이 home이고 확장자가 pug인 템플릿 파일을 찾아서 보여준다.
 
 export const search = (req, res) => {
   // 유저가 입력한 term을 가지고 오는 것 = req.query.term
   const {
-    query: { term : searchingBy } // term : searchingBy 이건 term에다 searchingBy라는 이름을 주는 것이다. 이제 searchingBy는 req.query.term과 같음
+    query: { term: searchingBy } // term : searchingBy 이건 term에다 searchingBy라는 이름을 주는 것이다. 이제 searchingBy는 req.query.term과 같음
   } = req; // const searchingBy = req.query.term; 예전 방식의 코딩
   res.render("search", { pageTitle: "검색결과", searchingBy });
 };
