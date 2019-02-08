e671ca1c654d01b977e5e64aa8b65f40b4c39ac0
fullyalive <wogud1202@gmail.com>
2019-02-08 22:21:29 +0700

    회원가입 로직 | 비밀번호 체크, 가입 성공시 홈으로 redirect

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,27 @@
+import routes from "../routes";
 // render 함수의 첫번째 인자는 template, 두 번째 인자는 템플릿에 추가할 정보가 담긴 객체
 
-export const signup = (req, res) => res.render("signup", { pageTitle: "회원가입" });
+export const getSignup = (req, res) => {
+  res.render("signup", { pageTitle: "회원가입" });
+};
+
+export const postSignup = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("signup", { pageTitle: "회원가입" });
+  } else {
+    // To Do : 사용자 등록
+    // To Do : 유저 로그인
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "로그인" });
-export const logout = (req, res) => res.render("logout", { pageTitle: "로그아웃" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "로그아웃" });
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "프로필수정" });
 export const changePassword = (req, res) =>
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,16 +1,22 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { signup, login, logout } from "../controllers/userController";
+import {
+  getSignup,
+  postSignup,
+  login,
+  logout
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 // method="get" 요청을 처리하는 컨트롤러
+globalRouter.get(routes.signup, getSignup);
+globalRouter.post(routes.signup, postSignup); // method가 post인 /signup 경로에서 작동한다.
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.signup, signup);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
-
 
 export default globalRouter;
