606147b329c9a331151d415a1ee7a7d896b01634
fullyalive <wogud1202@gmail.com>
2019-02-10 18:17:29 +0700

    Dot Env 구성하기

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config(); // .env 파일 안에 있는 정보를 불러온다. 그리고 찾은 모든 Variable들을 process.env.key에 저장한다.
 
-mongoose.connect("mongodb://localhost:27017/youtube", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false
 }); // 여기서 우리에게 요청하는 것은 string으로 된 DB이다. (어디에 DB가 저장되어있는지 알려주는 것)
