f497168b1c62b515d73ae365ac5aeea2e7c1be08
fullyalive <wogud1202@gmail.com>
2019-02-12 14:01:21 +0700

    메인 홈페이지 scss 작업
    flew-wrap : wrap 한 줄에 5개의 비디오를 출력

--- init.js
+++ init.js
@@ -1,9 +1,10 @@
-import "./db";
-import app from "./app";
 import dotenv from "dotenv";
-dotenv.config();
+import app from "./app";
+import "./db";
 import "./models/Video";
 import "./models/Comment";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
--- middlewares.js
+++ middlewares.js
@@ -8,7 +8,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "YouTube";
   res.locals.routes = routes; // routes 객체 추가 : 이후 링크주소로 routes.signup 이런식으로 사용 가능
   res.locals.user = {
-    isAuthenticated: false,
+    isAuthenticated: true,
     id: 1
   };
   next(); // 미들웨어가 next에 req를 전달해야 한다. app.js에서는 미들웨어가(localsMiddleware) 커넥션과 라우트들 사이에 있으니까 next()
