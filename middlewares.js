246657c6547109a50ae5de0fce9368374932b4e4
fullyalive <wogud1202@gmail.com>
2019-02-12 11:48:00 +0700

    Footer, Signup scss 작업

--- middlewares.js
+++ middlewares.js
@@ -8,10 +8,10 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "YouTube";
   res.locals.routes = routes; // routes 객체 추가 : 이후 링크주소로 routes.signup 이런식으로 사용 가능
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1
   };
   next(); // 미들웨어가 next에 req를 전달해야 한다. app.js에서는 미들웨어가(localsMiddleware) 커넥션과 라우트들 사이에 있으니까 next()
 };
 
-export const uploadVideo = multerVideo.single('videoFile'); // single : 오직 하나의 파일만 upload, Name part는 videoUpload.pug의 file의 name="videoFile"에서 온 것F
+export const uploadVideo = multerVideo.single("videoFile"); // single : 오직 하나의 파일만 upload, Name part는 videoUpload.pug의 file의 name="videoFile"에서 온 것F
