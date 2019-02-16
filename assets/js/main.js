024d5c8dd71d9b9e6095e2798b12217879c73a1e
fullyalive <wogud1202@gmail.com>
2019-02-16 23:23:55 +0700

    Video Player 커스터마이징 | 재생&정지 버튼 구현

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,2 @@
 import "../scss/style.scss";
-
-const something = async () => {
-  console.log("something");
-};
+import "./videoPlayer";
---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,21 @@
+const videoContainer = document.getElementById("jsVideoPlayer");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
+const playBtn = document.getElementById("jsPlayButton");
+
+function handlePlayClick() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  } else {
+    videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  }
+}
+
+function init() {
+  playBtn.addEventListener("click", handlePlayClick);
+}
+
+if (videoContainer) {
+  init();
+}
