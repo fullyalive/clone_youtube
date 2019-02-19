df55025dfdc55118e6bf1e53dc509d83c4d6cc75
fullyalive <wogud1202@gmail.com>
2019-02-19 16:25:36 +0700

    비디오 녹화하기
    이슈 : video duration

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,34 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "다시 녹화하기";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  // dataavailable 이벤트는 레코딩이 멈췄을 때 호출이 일어난다. -> 레코딩이 끝났을 때 전체 파일을 얻을 수 있음
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,14 +38,19 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "녹화 종료";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "녹화를 할 수 없어요 🤔";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    // try나 catch 중 하나가 실행되면 eventListener를 제거한다.
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
