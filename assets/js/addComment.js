f15dd81b2e55c4492891e14c7586ecb96dc3ef8a
fullyalive <wogud1202@gmail.com>
2019-02-20 00:01:18 +0700

    실시간 댓글내용, 댓글 수 반영

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1; // commentNumber가 string이기 때문에 parseInt로 숫자로 변환
+};
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -11,7 +25,9 @@ const sendComment = async comment => {
       comment // commentController의 postAddComment - body: { comment }안으로 들어간다
     }
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
 };
 
 const handleSubmit = event => {
